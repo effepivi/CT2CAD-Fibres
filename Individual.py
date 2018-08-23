035afdc88d7ff04321157d8d5ade9eb8738d8f18
zedbluffer <zedbluffer@dcc1ed12-c154-4d42-a5ce-f12ba5cd3162>
2018-08-23 14:02:08 +0000

    Use private attributes.

--- Individual.py
+++ Individual.py
@@ -3,15 +3,15 @@ import copy
 from FlyAlgorithm import *
 
 class Individual:
-    m_number_of_genes = 0;
-    m_fitness_function = 0;
+    __number_of_genes = 0;
+    __fitness_function = 0;
     m_fly_algorithm = 0;
 
     # Default and copy constructor
     def __init__(self, ind_id=-1, other=None):
-        self.m_id = ind_id;
-        self.m_fitness = 0;
-        self.m_is_active = True;
+        self.__id = ind_id;
+        self.__fitness = 0;
+        self.__is_active = True;
         
         if other is None:
             self.defaultConstructor();
@@ -19,34 +19,34 @@ class Individual:
             self.copyConstructor(other);
     
     def defaultConstructor(self):
-        if (Individual.m_number_of_genes):
+        if (Individual.__number_of_genes):
             self.createRandomIndividual();
     
     def copyConstructor(self, anIndividual):
-        self.m_p_gene_set = copy.deepcopy(anIndividual.m_p_gene_set)
-        self.m_is_active  = anIndividual.m_is_active;
+        self.__p_gene_set = copy.deepcopy(anIndividual.__p_gene_set)
+        self.__is_active  = anIndividual.__is_active;
     
     def createRandomIndividual(self):
-        self.m_p_gene_set = [random.uniform(0.5-0.275,0.5+0.275) for _ in range(Individual.getNumberOfGenes())];
+        self.__p_gene_set = [random.uniform(0.5-0.275,0.5+0.275) for _ in range(Individual.getNumberOfGenes())];
     
     def setNumberOfGenes(n):
-        Individual.m_number_of_genes = n;
+        Individual.__number_of_genes = n;
 
     def setFitnessFunction(f):
-        Individual.m_fitness_function = f;
+        Individual.__fitness_function = f;
 
     def getNumberOfGenes(self):
-        return (Individual.m_number_of_genes);
+        return (Individual.__number_of_genes);
         
     def getNumberOfGenes():
-        return (Individual.m_number_of_genes);
+        return (Individual.__number_of_genes);
 
     def computeFitness(self):
-        self.m_fitness = Individual.m_fitness_function(self.m_id, self.m_p_gene_set, Individual.m_fly_algorithm);
-        return self.m_fitness;
+        self.__fitness = Individual.__fitness_function(self.__id, self.__p_gene_set, Individual.m_fly_algorithm);
+        return self.__fitness;
         
     def print(self):
-        print(self.m_p_gene_set, self.m_fitness, self.m_is_active);
+        print(self.__p_gene_set, self.__fitness, self.__is_active);
     
     def immigration(self):
         self.createRandomIndividual();
@@ -55,15 +55,26 @@ class Individual:
         self.createRandomIndividual();
             
     def mutate(self, mu):
-        for i in range(len(self.m_p_gene_set)):
-            self.m_p_gene_set[i] += random.gauss(0.0, mu);
+        for i in range(len(self.__p_gene_set)):
+            self.__p_gene_set[i] += random.gauss(0.0, mu);
     
     def crossOver(self, ind1, ind2):
-        for i in range(len(self.m_p_gene_set)):
-            self.m_p_gene_set[i] = self.m_p_gene_set[i] * ind1.m_p_gene_set[i] + (1.0 - self.m_p_gene_set[i]) * ind2.m_p_gene_set[i]                        
+        for i in range(len(self.__p_gene_set)):
+            mu = random.gauss(0.0, 1.0)
+            self.__p_gene_set[i] = mu * ind1.__p_gene_set[i] + (1.0 - mu) * ind2.__p_gene_set[i]                        
         
     def isBetterThan(self, anIndividual):
-        return (self.m_fitness > anIndividual.m_fitness);
+        return (self.__fitness > anIndividual.__fitness);
         
     def getGene(self, i):
-        return (self.m_p_gene_set[i]);
+        return (self.__p_gene_set[i]);
+
+    def isActive(self):
+        return (self.__is_active);
+
+    def activate(self):
+        self.__is_active = True;
+    
+    def deactivate(self):
+        self.__is_active = False;
+
