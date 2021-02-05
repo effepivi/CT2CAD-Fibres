f71d39b27595691209d0699c3675d096de851b4c
Dr Franck P. Vidal <franck.p.vidal@fpvidal.net>
2021-02-05 09:07:28 +0000

    Add results for registration-matrix-then-fibres-SCW.

---
+++ registration-matrix-then-fibres-SCW/run_SCW_15/job_15.sh
@@ -0,0 +1,5 @@
+export EGL_PLATFORM=
+export PYTHONPATH=/home/fpvidal/gvirtualxray-install/gvxrWrapper-1.0.4/python3:/home/fpvidal/gvirtualxray-install/gvxrWrapper-1.0.4/python3:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/src:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/src:/home/fpvidal/gvirtualxray-install/gvxrWrapper-1.0.4/python3:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/src:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/src:
+date > validation-local/runtime_15
+python3 validation-low-tol.py --input Tutorial2/sino.raw --output validation-local/run_SCW_15/
+date >> validation-local/runtime_15
