c44122e5faf70b461eb59435ef40ec8f8883b939
Franck Vidal <franck.p.vidal@gmail.com>
2021-04-07 10:53:31 +0100

    Remove old results.

--- registration-matrix-then-fibres-SCW/run_SCW_9/job_9.sh
+++
@@ -1,5 +0,0 @@
-export EGL_PLATFORM=
-export PYTHONPATH=/home/fpvidal/gvirtualxray-install/gvxrWrapper-1.0.4/python3:/home/fpvidal/gvirtualxray-install/gvxrWrapper-1.0.4/python3:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/src:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/src:/home/fpvidal/gvirtualxray-install/gvxrWrapper-1.0.4/python3:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/Optimisation-algorithm-examples/src:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/examples:/home/fpvidal/PROGRAMMING/BACKUP/Optimisation-algorithm-examples/src:
-date > validation-local/runtime_9
-python3 validation-low-tol.py --input Tutorial2/sino.raw --output validation-local/run_SCW_9/
-date >> validation-local/runtime_9
