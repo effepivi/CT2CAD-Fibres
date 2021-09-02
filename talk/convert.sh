3f66629588f8c80a6a7a3222fcfcc646fe2db587
effepivi <franck.p.vidal@gmail.com>
2021-09-02 17:12:11 +0100

    Add talk.

---
+++ talk/convert.sh
@@ -0,0 +1,15 @@
+jupyter \
+    nbconvert \
+    talk-IBSim-4i_2021_fvidal.ipynb \
+    --to markdown \
+    --TagRemovePreprocessor.remove_cell_tags no_markdown \
+    --output build/talk-IBSim-4i_2021_fvidal.md
+
+
+pandoc \
+    -o build/talk.tex \
+    build/talk-IBSim-4i_2021_fvidal.md
+
+#--listings
+jupyter nbconvert --execute talk-IBSim-4i_2021_fvidal.ipynb --to slides --post serve
+
