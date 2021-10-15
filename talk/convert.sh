c9755428910da7d5acd6b59e791c194d95dd2782
Dr Franck P. Vidal <franck.p.vidal@fpvidal.net>
2021-10-15 17:58:39 +0100

    Update the results.

--- talk/convert.sh
+++ talk/convert.sh
@@ -1,14 +1,14 @@
-jupyter \
-    nbconvert \
-    talk-IBSim-4i_2021_fvidal.ipynb \
-    --to markdown \
-    --TagRemovePreprocessor.remove_cell_tags no_markdown \
-    --output build/talk-IBSim-4i_2021_fvidal.md
+#jupyter \
+#    nbconvert \
+#    talk-IBSim-4i_2021_fvidal.ipynb \
+#    --to markdown \
+#    --TagRemovePreprocessor.remove_cell_tags no_markdown \
+#    --output build/talk-IBSim-4i_2021_fvidal.md
 
 
-pandoc \
-    -o build/talk.tex \
-    build/talk-IBSim-4i_2021_fvidal.md
+#pandoc \
+#    -o build/talk.tex \
+#    build/talk-IBSim-4i_2021_fvidal.md
 
 #--listings
 jupyter nbconvert --execute talk-IBSim-4i_2021_fvidal.ipynb --to slides --post serve
